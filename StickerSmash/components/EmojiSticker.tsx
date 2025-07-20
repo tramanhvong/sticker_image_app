e4e9d35404e5843c3f0cb00cf5713be0dfa63649
Ngoc Tram Anh Vong <vongta21@my.yorku.ca>
2025-07-20 17:46:27 -0400

    add drag gesture

--- StickerSmash/components/EmojiSticker.tsx
+++ StickerSmash/components/EmojiSticker.tsx
@@ -10,6 +10,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     const scaleImage = useSharedValue(imageSize);
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
 
     const doubleTap = Gesture.Tap()
         .numberOfTaps(2)
@@ -28,15 +30,36 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
         };
     });
 
+    const drag = Gesture.Pan().onChange(event => {
+        translateX.value += event.changeX;
+        translateY.value += event.changeY;
+    });
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+            {
+                translateX: translateX.value,
+            },
+            {
+                translateY: translateY.value,
+            },
+            ],
+        };
+    });
+
     return (
-    <View style={{ top: -350 }}>
-        <GestureDetector gesture={doubleTap}>
-            <Animated.Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{width: imageSize, height: imageSize}}
-            />
-        </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+        <Animated.View style={[containerStyle, {top:-350}]}>
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={{width: imageSize, height: imageSize}}
+                />
+            </GestureDetector>
+        </Animated.View>
+    </GestureDetector>
+
   );
 }
