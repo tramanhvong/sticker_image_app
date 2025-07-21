38360594201ad9ee54dcdc495acb5a081ff10494
Ngoc Tram Anh Vong <vongta21@my.yorku.ca>
2025-07-21 10:34:32 -0400

    icon and splash arts

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,12 +1,17 @@
 /** defines shared UI elements such as headers and tab bar so they are consistent between different routes */
 import { Stack } from "expo-router";
-
+import { StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
   return (
+    <>
     <Stack>
     /**Stack navigator for navigating between different screens in an app**/
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    </Stack>   
+    <StatusBar style="light"/>
+    </>
+    
+
   );
 }
